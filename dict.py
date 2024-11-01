d7c8ce631c645cc7833b44c5bd8c24d4b60c26b9
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-11-01 18:49:52 +0100

    1.11.2024

---
+++ dict.py
@@ -0,0 +1,14 @@
+# # список
+# family_1 = ["Alex", "Olga", "Semen", "Nasty", "Alex"]
+# print(family_1)
+#
+#
+# # множества
+# family_2 = {"Alex", "Olga", "Semen", "Nasty", "alex"}
+# print(family_2)
+
+# словарь (ключ: значение)
+family_3 = {"Папа": "Alex", "Мама": "Olga", "Сын": "Semen", "Дочь": "Nasty"}
+#print(family_3["Папа"])
+for k, v in family_3.items():
+    print(k + " - " + v)
