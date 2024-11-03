b9f3e007ab8aa4563d5edcf124e0a6af7725f84a
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-11-03 19:11:23 +0100

    people2 03.11.2024

--- people.py
+++ people.py
@@ -21,9 +21,32 @@ class Person():
         """Изменение веса человека"""
         self.weight = kg
 
+class Warrior(Person):
+    """Создаем класс война"""
 
-man = Person("Alex", 30, 180)
+    def __init__(self, name, age, height):
+        """Инициализируем атрибуты класса родителя"""
+        super().__init__(name, age, height)
+        self.rage = 100
+
+    def get_rage(self):
+        """Получение заряда ярости"""
+        print("Заряд ярости равен : " + str(self.rage))
+
+    def description_person(self):
+        """Переопределения метода родителя"""
+        description = self.name + ", ему " + str(self.age) + ", его заряд ярости " + str(self.rage)
+        # print("Нового война зовут: " + description)
+        return description
+
+
+warrior = Warrior("Conan", 32, 200)
+# # warrior.get_rage()
+# # warrior.update_weight(150)
+# warrior.description_person()
+print("Нового война зовут: " + warrior.description_person())
+# man = Person("Alex", 30, 180)
 # man.description_person()
-# man.weight = 110
-man.update_weight(90)
-man.get_weight()
+# # man.weight = 110
+# man.update_weight(90)
+# man.get_weight()
