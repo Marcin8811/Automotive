fffc0c71af432d0108722ecc3a47afb0b75e582c
Marcin8811 <150076087+Marcin8811@users.noreply.github.com>
2024-11-10 14:41:21 +0100

    get

--- chuck_random.py
+++ chuck_random.py
@@ -1,15 +1,12 @@
 import requests
 
-class Test_new_joke:
+class TestNewJoke:
     """Создание новой шутки"""
 
-    def __init__(self):
-        pass
-
-    def test_create_new_random_joke(self):
-        """Создание случайной шутки"""
-
-        url = "https://api.chucknorris.io/jokes/random"
+    def test_create_new_random_categories_joke(self):
+        """Создание случайной шутки на определенную тему"""
+        category = "sport"
+        url = "https://api.chucknorris.io/jokes/random?category=" + category
         print(url)
         result = requests.get(url)
         print("Статус код : " + str(result.status_code))
@@ -17,23 +14,16 @@ class Test_new_joke:
         if result.status_code == 200:
             print("Успех! Мы получили новую шутку!")
         else:
-            print("Все херня, давай по новой.")
+            print("Что-то пошло не так, давай по новой.")
         result.encoding = 'utf-8'
         print(result.text)
         check = result.json()
-        # check_info = check.get("categories")
-        # print(check_info)
-        # assert check_info == []
-        # print("Категория верна")
-        check_info_value = check.get("value")
-        print(check_info_value)
-        name = "Chuck"
-        if name in check_info_value:
-            print("Chuck присутствует")
-        else:
-            print("Chuck отсутствует")
-
+        check_info = check.get("categories")
+        print(check_info)
+        assert check_info == ["sport"]
+        print("Категория верна")
 
 if __name__ == "__main__":
-    random_joke = Test_new_joke()
-    random_joke.test_create_new_random_joke()
+    # Запуск теста
+    test_instance = TestNewJoke()
+    test_instance.test_create_new_random_categories_joke()
